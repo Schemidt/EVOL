2af72f6f04b152894c5ad183476eb2cd4b8a9bc8
Schemidt <alexeySamson@yandex.ru>
2018-06-28 11:26:52 +0300

    remove debug code

--- BASE/BASE.cpp
+++ BASE/BASE.cpp
@@ -576,7 +576,7 @@ int main(int argc, char *argv[])
 	Sound *shock[4][50] = { nullptr };
 	Sound *trim = nullptr;
 	Sound *frict = nullptr;
-	
+
 	SOUNDREAD localdata = soundread;//локальная копия общего с USPO файла
 
 	double timerPodk = 0;
@@ -630,7 +630,6 @@ int main(int argc, char *argv[])
 	double noDataFactor = 0;//Индикатор прекращения поступления данных
 	bool stop = 0;//Признак крушения
 
-	int a = 0;int b = 0;
 	//Опрашиваем все блоки программы в бесконечном цикле
 	while (true)
 	{
@@ -674,7 +673,7 @@ int main(int argc, char *argv[])
 
 			//printf(" Time: %.4lf\tDT__: %.4lf\tSIU: %i\tEIU: %i\r", Sound::currentTime, avrDeltaTime, Sound::sourcesInUse, Sound::effectSlotsInUse);
 
-			//printf(" Time: %.4lf\tDT__: %.4lf\tENG1: %.3f\tENG2: %.3f\tRED_: %.3f\tVSU: %.3f\tMSG: %.3f\t\r", Sound::currentTime, avrDeltaTime, soundread.eng1_obor, soundread.eng2_obor, soundread.reduktor_gl_obor, soundread.vsu_obor, Sound::masterGain);
+			printf(" Time: %.4lf\tDT__: %.4lf\tENG1: %.3f\tENG2: %.3f\tRED_: %.3f\tVSU: %.3f\tMSG: %.3f\t\r", Sound::currentTime, avrDeltaTime, soundread.eng1_obor, soundread.eng2_obor, soundread.reduktor_gl_obor, soundread.vsu_obor, Sound::masterGain);
 
 			//Среднее время цикла
 			avrDeltaTime = 0;
@@ -1555,16 +1554,12 @@ int main(int argc, char *argv[])
 						{
 							if (rand() % 10 > 5)
 							{
-								type[i] = helicopter.fullName["bullet0"]; a++;
+								type[i] = helicopter.fullName["bullet0"];
 							}
 							else
 							{
-								type[i] = helicopter.fullName["bullet1"]; b++;
+								type[i] = helicopter.fullName["bullet1"];
 							}
-						}
-						else
-						{
-							
 						}
 
 						bullet[i]->play(bulletHitLocker, type[i], "NULL", "NULL", helicopter.bulletHitFactor);
@@ -2203,8 +2198,6 @@ int main(int argc, char *argv[])
 					}
 				}
 			}
-
-			cout << a << " " << b << "\r";
 		}
 		else
 		{
